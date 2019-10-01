d10bb43c6942420ef88484a1b5564138d13c1831
Leo Dion <leogdion@brightdigit.com>
2019-10-01 10:04:29 -0400

    Initial Commit

---
+++ rp-save-data-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rp-save-data-app
+//
+//  Created by Leo Dion on 10/1/19.
+//  Copyright © 2019 BrightDigit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
